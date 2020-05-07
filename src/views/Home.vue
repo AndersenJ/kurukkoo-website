891918befd427afa567d5f627c05c8900c6d9540
AndersenJ <johnathan.andersen@byu.edu>
2020-05-07 17:35:21 -0600

    did some basic work to get the first page to display on the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Page chapter="00" pagenum="000"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Page from '@/components/Page.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Page
   }
 }
 </script>
