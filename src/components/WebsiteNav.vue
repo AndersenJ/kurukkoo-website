c3ee299f974db37c198ec5cdc38f396ced287214
AndersenJ <johnathan.andersen@byu.edu>
2020-05-08 15:16:01 -0600

    added patreon link

--- src/components/WebsiteNav.vue
+++ src/components/WebsiteNav.vue
@@ -3,6 +3,7 @@
         <router-link to="/">Home</router-link>
         <router-link to="/archive">Archive</router-link>
         <router-link to="/about">About</router-link>
+        <a href="https://www.patreon.com/kadew">Patreon</a>
     </div>
 </template>
 
