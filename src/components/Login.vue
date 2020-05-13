b6f3c81ff52524697203c584b03e1604c3ccc17e
AndersenJ <johnathan.andersen@byu.edu>
2020-05-13 16:14:17 -0600

    added basic honeypot for login/register. fixed minor registration bug

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,12 +2,14 @@
     <div>
         <form>
                 <input placeholder="email" v-model="email">
+                <div style="display: none"><input type="text" name="Name" v-model="honeypot1" /></div>
                 <input placeholder="username" v-model="username">
                 <input type="password" placeholder="password" v-model="password">
                 <button type="submit" @click.prevent="register">Register</button>
         </form>
         <p v-if="error" class="error">{{error}}</p>
         <form>
+                <div style="display: none"><input type="text" name="Name" v-model="honeypot2" /></div>
                 <input placeholder="username" v-model="usernameLogin">
                 <input type="password" placeholder="password" v-model="passwordLogin">
                 <button type="submit" class="pure-button pure-button-primary" @click.prevent="login">Login</button>
@@ -22,6 +24,8 @@ export default {
     name: 'Login',
     data() {
         return {
+            honeypot1: '',
+            honeypot2: '',
             email: '',
             username: '',
             password: '',
@@ -33,37 +37,42 @@ export default {
     },
     methods: {
         async register() {
-            this.error = '';
-            this.errorLogin = '';
-            if (!this.email || !this.username || !this.password)
-                return;
-            try {
-                let response = await axios.post('/api/users', {
-                    email: this.email,
-                    username: this.username,
-                    password: this.password,
-                });
-                this.$root.$data.user = response.data.user;
-            } catch (error) {
-                this.error = error.response.data.message;
-                this.$root.$data.user = null;
+            if (this.honeypot1 === '' && this.honeypot1 === '') {
+                this.error = '';
+                this.errorLogin = '';
+                if (!this.email || !this.username || !this.password)
+                    return;
+                try {
+                    let response = await axios.post('/api/users', {
+                        email: this.email,
+                        username: this.username,
+                        password: this.password,
+                    });
+                    this.$root.$data.user = response.data.user;
+                    this.$emit('check-user');
+                } catch (error) {
+                    this.error = error.response.data.message;
+                    this.$root.$data.user = null;
+                }
             }
         },
         async login() {
-            this.error = '';
-            this.errorLogin = '';
-            if (!this.usernameLogin || !this.passwordLogin)
-                return;
-            try {
-                let response = await axios.post('/api/users/login', {
-                    username: this.usernameLogin,
-                    password: this.passwordLogin,
-                });
-                this.$root.$data.user = response.data.user;
-                this.$emit('check-user');
-            } catch (error) {
-                this.errorLogin = "Error: " + error.response.data.message;
-                this.$root.$data.user = null;
+            if (this.honeypot1 === '' && this.honeypot1 === '') {
+                this.error = '';
+                this.errorLogin = '';
+                if (!this.usernameLogin || !this.passwordLogin)
+                    return;
+                try {
+                    let response = await axios.post('/api/users/login', {
+                        username: this.usernameLogin,
+                        password: this.passwordLogin,
+                    });
+                    this.$root.$data.user = response.data.user;
+                    this.$emit('check-user');
+                } catch (error) {
+                    this.errorLogin = "Error: " + error.response.data.message;
+                    this.$root.$data.user = null;
+                }
             }
         },
     },
